b61af3a5d19c47895c9987b0899780581704bd76
Aviel <avlevy@g.jct.ac.il>
2022-07-14 03:11:57 +0300

    Evolution pikatchu

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Routes from "./components/Routes";
+const App = () => {
+  return (
+    <div>
+      <div>He</div>
+      <Routes />
+    </div>
+  );
+};
+
+export default App;
