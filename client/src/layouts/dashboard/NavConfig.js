42657bb4a2ba37b31b5d167eb887d84ab1496014
mickaelbalensi <mickaelbalensi2652@gmail.com>
2022-07-25 10:19:25 +0200

    remove blog and product

--- client/src/layouts/dashboard/NavConfig.js
+++ client/src/layouts/dashboard/NavConfig.js
@@ -22,16 +22,6 @@ const navConfig = [
     icon: getIcon('eva:people-fill'),
   },
   {
-    title: 'product',
-    path: '/dashboard/products',
-    icon: getIcon('eva:shopping-bag-fill'),
-  },
-  {
-    title: 'blog',
-    path: '/dashboard/blog',
-    icon: getIcon('eva:file-text-fill'),
-  },
-  {
     title: 'login',
     path: '/login',
     icon: getIcon('eva:lock-fill'),
@@ -40,12 +30,7 @@ const navConfig = [
     title: 'register',
     path: '/register',
     icon: getIcon('eva:person-add-fill'),
-  },
-  {
-    title: 'Not found',
-    path: '/404',
-    icon: getIcon('eva:alert-triangle-fill'),
-  },
+  }
 ];
 
 export default navConfig;
